fa7de1f8fd7c7a2dda1702b80d7510bced67f65f
aTze91 <marcoatzeno@gmail.com>
2022-04-19 14:01:06 +0200

    Update main.py

--- while/main.py
+++ while/main.py
@@ -3,22 +3,28 @@ from helpers import random_koala_fact
 __winc_id__ = "c0dc6e00dfac46aab88296601c32669f"
 __human_name__ = "while"
 
-'''unique_koala_facts: takes an integer as an argument and returns that number of unique koala facts in a list. 
-Note that there are only a limited number of unique facts in the dataset. For high arguments your function should try to return all unique facts in the dataset.
- No worries: the number of facts is small enough that this should be feasible. You can set an iteration limit of 1000.'''
+'''unique_koala_facts: takes an integer as an argument and returns that 
+number of unique koala facts in a list.
+Note that there are only a limited number of unique facts in the dataset.
+For high arguments your function should try to return all unique facts in the dataset.
+No worries: the number of facts is small enough that this should be feasible. 
+You can set an iteration limit of 1000.'''
 
-def unique_koala_facts(n):
+
+def unique_koala_facts(n):  
     d = 0
     koala_facts = []
-    while n > 0 : 
-          fact_x = random_koala_fact()
-          if fact_x not in koala_facts : 
-                  koala_facts += [fact_x]
-                  n -= 1 
-          d += 1
-          if d == 1000: 
-             break
-    return koala_facts    
+    while n > 0: 
+        fact_x = random_koala_fact()
+        if fact_x not in koala_facts: 
+            koala_facts += [fact_x]
+            n -= 1 
+        d += 1
+        if d == 1000: 
+            break
+    return koala_facts   
+
+
 '''
 num_joey_facts: young marsupials are called 'joeys'. How many unique facts mentioning this term are in our facts dataset? 
 Count them by getting facts from random_koala_fact until you have seen some particular fact 10 times, then return how many unique joey facts you counted in the dataset.'''

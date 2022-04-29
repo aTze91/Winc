d25c18cc93da3509882293c8f3f37c54a0d4672a
aTze91 <marcoatzeno@gmail.com>
2022-04-29 15:47:12 +0200

    Update main.py

--- comments/main.py
+++ comments/main.py
@@ -13,17 +13,17 @@ brussel_sprout = 7
 leek = 2
 
 sum_one_each = broccoli + potato + brussel_sprout + leek
-avg_price = sum_one_each / 4 #calulating the average price
-# setting how many pieces of each product we will buy 
+avg_price = sum_one_each / 4  # calulating the average price
+# setting how many pieces of each product we will buy
 num_potatoes = 7
 num_broccolis = 5
 num_brussel_sprouts = 10
-num_leeks= 2
+num_leeks = 2
 
-sum_total= broccoli*num_broccolis + potato*num_potatoes + brussel_sprout*num_brussel_sprouts + leek*num_leeks
+sum_total = broccoli * num_broccolis + potato * num_potatoes + brussel_sprout * num_brussel_sprouts + leek * num_leeks
 discount_percentage = 30
-discounted_sum_total = sum_total - sum_total/100*discount_percentage #calculating the total cost with a 30% discount 
+discounted_sum_total = sum_total - sum_total / 100 * discount_percentage  # calculating the total cost with a 30% discount
 
-print (discounted_sum_total)
+print(discounted_sum_total)
 """ is been a nice dinner afterall,
     I like broccoli """
